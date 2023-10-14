03818844eec004db44d44402353ebdc799a1b21d
souzagio <eusougiovany@gmail.com>
2023-10-14 15:32:58 -0300

    Done

---
+++ Advanced/Lateral_Menu/scripts.js
@@ -0,0 +1,6 @@
+let btnExp = document.querySelector("#inc")
+let menu = document.querySelector(".menu-lateral")
+
+btnExp.addEventListener('click', function(){
+    menu.classList.toggle('expandir')
+})
